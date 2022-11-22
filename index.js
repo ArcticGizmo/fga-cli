108118482c02b0c1ecd6666566c3a55cbb467c4d
Jonathan Howell <jon.howell314@gmail.com>
2022-11-22 12:44:45 +0800

    added start and stop

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { Command } from "commander";
 import { list } from "./lib/list.js";
 import { loadConfig, loadModel, loadState } from "./lib/configuration.js";
+import { startInstance, stopInstance, logInstance } from "./lib/setup.js";
 
-const config = loadConfig();
+const c = loadConfig();
 const m = loadModel();
 const s = loadState();
-console.dir(s);
 
 const cli = new Command();
 
@@ -13,6 +13,21 @@ cli
   .name("fga-cli")
   .description("Help manage OpenFGA development instances")
   .version("0.0.1");
+
+cli
+  .command("start")
+  .description("Start a local OpenFGA docker instance")
+  .action(startInstance);
+
+cli
+  .command("stop")
+  .description("Stop local OpenFGA docker instance")
+  .action(stopInstance);
+
+cli
+  .command("logs")
+  .description("Stream logs from instance")
+  .action(logInstance);
 
 cli.command("list").description("List all available stores").action(list);
 
